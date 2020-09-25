453ed3553a3f948746d51043ae1bca11e27990a6
Ali Babaei <alibabaei@Alis-MacBook-Pro.local>
2020-09-25 15:29:31 -0700

    added the attack animation

--- Games/Player/Player.gd
+++ Games/Player/Player.gd
@@ -4,14 +4,34 @@ const MAX_SPEED = 50
 const ACCELARATION = 500
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-func _physics_process(delta):
+func _ready():
+	animationTree.active = true
 	
+func _physics_process(delta):
+	#Set up match statement 
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+	
+func move_state(delta):	
 	#make the character move
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -24,6 +44,7 @@ func _physics_process(delta):
 		
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
 		#to make the charactor accelerate: velocity += input_velocity * ACCELERATION * delta) make sure to use +=
@@ -32,3 +53,13 @@ func _physics_process(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+
+func attack_state(delta):
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
