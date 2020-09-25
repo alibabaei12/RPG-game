1d65344882d7d6b32548fd06d314634a552a6c38
Ali Babaei <alibabaei@Alis-MacBook-Pro.local>
2020-09-24 18:32:03 -0700

    first commit

---
+++ Games/Player/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 50 
+const ACCELARATION = 500
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
+func _physics_process(delta):
+	
+	#make the character move
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	#to make it so that the speed in every angle is the same
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELARATION * delta)
+		#to make the charactor accelerate: velocity += input_velocity * ACCELERATION * delta) make sure to use +=
+		
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = move_and_slide(velocity)
