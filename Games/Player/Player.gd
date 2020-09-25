8c47f432fb82cf3b777636c0da760b72e053eace
Ali Babaei <alibabaei@Alis-MacBook-Pro.local>
2020-09-25 16:20:14 -0700

    Added the grass effect

--- Games/Player/Player.gd
+++ Games/Player/Player.gd
@@ -20,7 +20,7 @@ onready var animationState = animationTree.get("parameters/playback")
 func _ready():
 	animationTree.active = true
 	
-func _physics_process(delta):
+func _process(delta):
 	#Set up match statement 
 	match state:
 		MOVE:
