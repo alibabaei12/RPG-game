46e377a12e858b5b006bd7bb23edf387b88a8630
Ali Babaei <alibabaei@Alis-MacBook-Pro.local>
2020-09-25 17:17:28 -0700

    can cut grass

--- Games/Effects/GrassEffect.gd
+++ Games/Effects/GrassEffect.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var animatedSprite = $AnimatedSprite
 
 func _ready():
-	animatedSprite.frame = 0
+	
 	animatedSprite.play("Animate")
 
 func _on_AnimatedSprite_animation_finished():
